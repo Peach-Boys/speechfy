abeffc0458d2e3d873ebe8a3718c5997d8c37e9b
PoeySK <piz115@naver.com>
2025-03-20 17:01:11 +0900

    feat: 악기 선택 시 다른 악기 리스트 나오는 UI구현
    현재 악기 Id를 기준으로 다른 악기 리스트가 나오도록 구현
    
    추후 악기 Id에 관한 내용이 정확히 나오면 정적인 값 변경 예정
    
    관련 이슈
    - S12P21B105-76

--- client/src/components/features/Track/InstrumentList.tsx
+++ client/src/components/features/Track/InstrumentList.tsx
@@ -9,17 +9,14 @@ interface Props {
 }
 
 function InstrumentList({ instrumentId }: Props) {
-  // 선택 테스트용 함수
-  function handleChangeInstrument(id: number) {
-    console.log('id:', id);
-  }
-
   return (
-    <menu className='flex flex-col gap-1'>
+    <menu
+      className={`flex flex-col gap-1 bg-white shadow-md rounded-full p-2 `}
+    >
       {instrumentId !== 0 && (
         <div
           className='size-12 flex justify-center items-center rounded-full bg-gray-300 cursor-pointer'
-          onClick={() => handleChangeInstrument(0)}
+          onClick={() => console.log('Piano 선택')}
         >
           <IconPiano />
         </div>
@@ -27,7 +24,7 @@ function InstrumentList({ instrumentId }: Props) {
       {instrumentId !== 1 && (
         <div
           className='size-12 flex justify-center items-center rounded-full bg-gray-300 cursor-pointer'
-          onClick={() => handleChangeInstrument(1)}
+          onClick={() => console.log('베이스 기타 선택')}
         >
           <IconBaseGuitar />
         </div>
@@ -35,7 +32,7 @@ function InstrumentList({ instrumentId }: Props) {
       {instrumentId !== 2 && (
         <div
           className='size-12 flex justify-center items-center rounded-full bg-gray-300 cursor-pointer'
-          onClick={() => handleChangeInstrument(2)}
+          onClick={() => console.log('드럼 선택')}
         >
           <IconDrum />
         </div>
@@ -43,7 +40,7 @@ function InstrumentList({ instrumentId }: Props) {
       {instrumentId !== 3 && (
         <div
           className='size-12 flex justify-center items-center rounded-full bg-gray-300 cursor-pointer'
-          onClick={() => handleChangeInstrument(3)}
+          onClick={() => console.log('일렉 기타 선택')}
         >
           <IconElecGuitar />
         </div>
--- client/src/components/features/Track/index.tsx
+++ client/src/components/features/Track/index.tsx
@@ -12,35 +12,31 @@ import { useRef, useState } from 'react';
 
 function Track() {
   const [isPlay, setIsPlay] = useState<boolean>(false);
-  const [isMenuOpen, setIsMenuOepn] = useState<boolean>(false);
-  const [isSelInstOpen, setIsSelIstOpen] = useState<boolean>(false);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [isSelInstOpen, setIsSelInstOpen] = useState<boolean>(false);
   const menuRef = useRef<HTMLDivElement | null>(null);
   const selectInstRef = useRef<HTMLDivElement | null>(null);
 
-  function handleChangeInstrument() {}
   function handlePlayTrack() {
     setIsPlay(!isPlay);
   }
 
   function handleOpenMenu() {
-    setIsMenuOepn((prev) => !prev);
+    setIsMenuOpen((prev) => !prev);
   }
 
-  if (menuRef) {
-    useOutSideClick(menuRef, () => setIsMenuOepn(false));
-  }
+  useOutSideClick(menuRef, () => setIsMenuOpen(false));
 
   function handleOpenSelInst() {
-    setIsSelIstOpen((prev) => !prev);
+    setIsSelInstOpen((prev) => !prev);
   }
-  if (selectInstRef) {
-    useOutSideClick(selectInstRef, () => setIsSelIstOpen(false));
-  }
+
+  useOutSideClick(selectInstRef, () => setIsSelInstOpen(false));
 
   return (
     <Box borderStyle='solid'>
       <div className='w-full flex flex-col gap-5'>
-        <div className='w-f flex justify-between items-center'>
+        <div className='w-full flex justify-between items-center'>
           <div className='flex items-center gap-2'>
             <IconDoubleCircle color='#ffffff' />
             <span>악기 이름</span>
@@ -51,22 +47,30 @@ function Track() {
           >
             <IconTripleDots color='#ffffff' />
             {isMenuOpen && (
-              <div ref={menuRef} className='absolute z-1 top-0 right-0'>
+              <div ref={menuRef} className='absolute z-10 top-0 right-0'>
                 <TrackMenu />
               </div>
             )}
           </div>
         </div>
         <div className='flex gap-4'>
-          <div className='relative w-full' onClick={handleOpenSelInst}>
-            <Button onClick={handleChangeInstrument}>악기 선택</Button>
+          <div className='relative w-full'>
+            <Button onClick={handleOpenSelInst}>악기 선택</Button>
             {isSelInstOpen && (
-              <div ref={selectInstRef} className='absolute z-1 pt-1'>
+              <div
+                ref={selectInstRef}
+                className='absolute z-10 pt-1 overflow-hidden'
+              >
                 <InstrumentList instrumentId={1} />
               </div>
             )}
           </div>
-          <Button onClick={handlePlayTrack}>트랙 재생</Button>
+          <Button
+            onClick={handlePlayTrack}
+            buttonStyle='btn-jihyegra text-white'
+          >
+            트랙 재생
+          </Button>
         </div>
         <WavePlay isPlay={isPlay} />
       </div>
