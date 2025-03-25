df774f81d2da19992c76af05a51fa500aaddc669
정진용 <e50792000@naver.com>
2025-03-25 13:36:26 +0900

    BE/feat: DTO 자료형 int -> Integer로 일부 수정

--- server/speechfy/src/main/java/com/ssafy/speechfy/dto/song/imageResponseDto.java
+++ server/speechfy/src/main/java/com/ssafy/speechfy/dto/song/imageResponseDto.java
@@ -10,5 +10,5 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class imageResponseDto {
-    private int id;
+    private Integer id;
 }
--- server/speechfy/src/main/java/com/ssafy/speechfy/dto/song/songDeleteDto.java
+++ server/speechfy/src/main/java/com/ssafy/speechfy/dto/song/songDeleteDto.java
@@ -10,5 +10,5 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class songDeleteDto {
-    private int id;
+    private Integer id;
 }
