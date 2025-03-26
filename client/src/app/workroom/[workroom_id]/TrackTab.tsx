2fa41867b79203eab6e103c1654d4454b349b879
이해루 <gofn080776@gmail.com>
2025-03-26 21:48:17 +0900

    feat: 트랙 전체 재생 구현

--- client/src/app/workroom/[workroom_id]/TrackTab.tsx
+++ client/src/app/workroom/[workroom_id]/TrackTab.tsx
@@ -15,9 +15,23 @@ const DUMMYTRACKS: ITrack[] = [
     trackName: 'SoundHelix-Song-1',
     trackUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
   },
+  {
+    trackId: 1,
+    instrumentName: 'SoundHelix-Song-1',
+    isPlaying: false,
+    trackName: 'SoundHelix-Song-1',
+    trackUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+  },
 ];
 function TrackTab() {
   const [tracks, setTracks] = useState<ITrack[]>(DUMMYTRACKS);
+  const [isAllPlay, setIsAllPlay] = useState<boolean>(false);
+  function handlePlayAll() {
+    setIsAllPlay(true);
+  }
+  function handleAllStop() {
+    setIsAllPlay(false);
+  }
   return (
     <div className='w-full h-full min-h-4/5 max-h-5/6 p-5 flex flex-col items-center gap-3'>
       <div className='w-full h-fit flex justify-between'>
@@ -25,15 +39,18 @@ function TrackTab() {
           <IconReset />
           <span>초기화</span>
         </button>
-        <button className='flex items-center gap-1'>
+        <button
+          className='flex items-center gap-1'
+          onClick={isAllPlay ? handleAllStop : handlePlayAll}
+        >
           <IconAllPlay color='#ffffff' />
-          <span>전체 재생</span>
+          <span>{isAllPlay ? '재생 중지' : '전체 재생'}</span>
         </button>
       </div>
       <div className='w-full h-fit flex flex-col items-center gap-2 overflow-auto'>
         {/* UI 테스트용 Track 여러 개 생성 */}
         {tracks.map((track) => (
-          <Track key={track.trackId} track={track} />
+          <Track key={track.trackId} track={track} isAllPlay={isAllPlay} />
         ))}
         <CreateRecord setTracks={setTracks} tracks={tracks} />{' '}
         {/* 임시로 빈 배열 입력 */}
