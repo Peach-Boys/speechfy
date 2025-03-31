0bff4e47a3ecad03db6fc049ec605449b87d8a65
정진용 <e50792000@naver.com>
2025-03-31 15:07:42 +0900

    Be/fix : S3controller 수정

--- server/speechfy/src/main/java/com/ssafy/speechfy/controller/S3Controller.java
+++ server/speechfy/src/main/java/com/ssafy/speechfy/controller/S3Controller.java
@@ -9,10 +9,7 @@ import com.ssafy.speechfy.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,30 +20,13 @@ public class S3Controller {
     private final RecordReposiotry recordReposiotry;
     private final SongRepository songRepository;
     private final TrackRepository trackRepository;
-    @GetMapping("/track/{trackId}")
-    public ResponseEntity<PresignedUrlDto> getTrackUrl(@PathVariable Long trackId) {
-        String objectKey = "users/1/track" + trackId;
+    @GetMapping("/presignedUrl")
+    public ResponseEntity<PresignedUrlDto> getUrl(@RequestParam(value = "category", required = false) String category) {
+        String objectKey = "users/1/" + category;
         PresignedUrlDto responseDto = new PresignedUrlDto(
                 s3Service.generatePresignedUrl(objectKey).toString()
         );
         return ResponseEntity.ok(responseDto);
     }
 
-    @GetMapping("/song/{songId}")
-    public ResponseEntity<PresignedUrlDto> getSongUrl(@PathVariable Long songId) {
-        String objectKey = "users/1/track/1" + songId;
-        PresignedUrlDto responseDto = new PresignedUrlDto(
-                s3Service.generatePresignedUrl(objectKey).toString()
-        );
-        return ResponseEntity.ok(responseDto);
-    }
-
-    @GetMapping("/record/{recordId}")
-    public ResponseEntity<PresignedUrlDto> getRecordUrl(@PathVariable Long recordId) {
-        String objectKey = "users/1" + recordId;
-        PresignedUrlDto responseDto = new PresignedUrlDto(
-                s3Service.generatePresignedUrl(objectKey).toString()
-        );
-        return ResponseEntity.ok(responseDto);
-    }
 }
