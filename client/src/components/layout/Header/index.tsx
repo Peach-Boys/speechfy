918b78bc27a2793ac1e04a8018f803e024fc1c12
강성엽 <piz115@naver.com>
2025-04-02 11:08:10 +0900

    feat: Header 햄버거 메뉴 추가
    로그아웃은 추후 구현되면 구현
    
    관련 이슈
    - S12P21B105-101

--- client/src/components/layout/Header/index.tsx
+++ client/src/components/layout/Header/index.tsx
@@ -1,13 +1,18 @@
+import Hamburger from '@/components/common/Hamburger';
 import IconPeachPeach from '@/components/icons/IconPeachPeach';
 
 function Header() {
   return (
-    <header className='fixed w-full h-[60px] max-w-[500px] px-3 flex items-center gap-3 bg-black z-100'>
-      <div className='w-[40px] h-[40px] flex justify-center items-center rounded-full bg-jihyegra'>
-        <IconPeachPeach width={30} height={30} />
+    <header className='fixed w-full h-[60px] px-3 flex items-center bg-black z-100'>
+      <div className='flex items-center'>
+        <div className='w-[40px] h-[40px] flex justify-center items-center rounded-full bg-jihyegra'>
+          <IconPeachPeach width={30} height={30} />
+        </div>
+        <span className='font-[SEBANG] text-3xl font-bold '>SPEECHFY</span>
       </div>
-      <span className='font-[SEBANG] text-3xl font-bold '>SPEECHFY</span>
-      {/* 햄버거 메뉴는 추후 개발 */}
+      <div className='ml-auto'>
+        <Hamburger />
+      </div>
     </header>
   );
 }
