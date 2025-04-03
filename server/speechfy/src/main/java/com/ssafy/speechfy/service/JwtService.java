f65c8db55029c642866f28e4a80377bba6838208
고대권 <konzo321@naver.com>
2025-04-03 10:43:48 +0900

    HOTFIX: 에러 위치 확인용 log 생성 추가

--- server/speechfy/src/main/java/com/ssafy/speechfy/service/JwtService.java
+++ server/speechfy/src/main/java/com/ssafy/speechfy/service/JwtService.java
@@ -48,14 +48,15 @@ public class JwtService {
     }
 
     public String generateAccessToken(User user) throws JOSEException {
+        log.info("Generate access token");
         JWSHeader header = new JWSHeader.Builder(this.jwtKeyService.getJwsAlgorithm())
                 .type(JOSEObjectType.JWT)
                 .build();
-
+        log.info("Generate access token2");
         Instant expiresAt = Instant.now()
                 .plusSeconds(ACCESS_EXPIRES_IN_SECONDS)
                 .truncatedTo(ChronoUnit.SECONDS);
-
+        log.info("Generate access token3");
         Payload payload = new JWTClaimsSet.Builder()
                 .issuer(JWT_ISSUER)
                 .subject(String.valueOf(user.getId()))
@@ -63,19 +64,20 @@ public class JwtService {
                 .claim(CLAIM_TYPE, TYPE_ACCESS)
                 .build()
                 .toPayload();
-
+        log.info("Generate access token4");
         return this.jwsMinter.mint(header, payload, null).serialize();
     }
 
     public String generateRefreshToken(User user) throws JOSEException {
+        log.info("Generate refresh token1");
         JWSHeader header = new JWSHeader.Builder(this.jwtKeyService.getJwsAlgorithm())
                 .type(JOSEObjectType.JWT)
                 .build();
-
+        log.info("Generate refresh token2");
         Instant expiresAt = Instant.now()
                 .plusSeconds(REFRESH_EXPIRES_IN_SECONDS)
                 .truncatedTo(ChronoUnit.SECONDS);
-
+        log.info("Generate refresh token3");
         Payload payload = new JWTClaimsSet.Builder()
                 .issuer(JWT_ISSUER)
                 .subject(String.valueOf(user.getId()))
@@ -83,7 +85,7 @@ public class JwtService {
                 .claim(CLAIM_TYPE, TYPE_REFRESH)
                 .build()
                 .toPayload();
-
+        log.info("Generate refresh token4");
         return this.jwsMinter.mint(header, payload, null).serialize();
     }
 }
