4dab58ab77282bce4ac996c431c2124dc28615db
고대권 <konzo321@naver.com>
2025-04-03 10:50:43 +0900

    HOTFIX: 로그 보기 기능 수정

--- server/speechfy/src/main/java/com/ssafy/speechfy/service/JwtService.java
+++ server/speechfy/src/main/java/com/ssafy/speechfy/service/JwtService.java
@@ -16,6 +16,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.time.Instant;
