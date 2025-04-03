4c2648a7969c7bef2b38ad826040db6eaeff0d55
PoeySK <piz115@naver.com>
2025-04-04 01:06:54 +0900

    chore: 불필요한 console.log 삭제

--- client/src/service/apis/Share.ts
+++ client/src/service/apis/Share.ts
@@ -3,9 +3,7 @@ import { IShareSong } from '@/types/song';
 
 export const getShareSong = async (songId: number): Promise<IShareSong> => {
   try {
-    console.log('songId 요청:', songId);
     const res = await client.get(`/song/share/${songId}`);
-    console.log('res:', res.data);
     return res.data;
   } catch (err: unknown) {
     throw new Error((err as Error).message);
