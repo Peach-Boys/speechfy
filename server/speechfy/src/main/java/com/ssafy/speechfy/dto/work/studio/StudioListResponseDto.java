69b6bea3f7fdb2f61aed6f27c2066d77ce996b67
정진용 <e50792000@naver.com>
2025-04-03 16:20:16 +0900

    fix: workList -> studioList

--- server/speechfy/src/main/java/com/ssafy/speechfy/dto/work/studio/StudioListResponseDto.java
+++ server/speechfy/src/main/java/com/ssafy/speechfy/dto/work/studio/StudioListResponseDto.java
@@ -12,5 +12,5 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudioListResponseDto {
-    private List<StudioSimpleDto> workList;
+    private List<StudioSimpleDto> studioList;
 }
