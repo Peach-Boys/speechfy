77699ea52a7633cf881b86b08fa51f4b87749506
정윤선 <99yunsun@naver.com>
2025-04-07 15:19:24 +0900

    fix: chagned type of mood and genre String to MoodType/GenreType, and added instruments properties

--- server/speechfy/src/main/java/com/ssafy/speechfy/dto/song/BasicSongRegisterRequestDto.java
+++ server/speechfy/src/main/java/com/ssafy/speechfy/dto/song/BasicSongRegisterRequestDto.java
@@ -1,6 +1,10 @@
 package com.ssafy.speechfy.dto.song;
 
+import com.ssafy.speechfy.enums.GenreType;
+import com.ssafy.speechfy.enums.MoodType;
 import lombok.*;
+
+import java.util.List;
 
 @Builder
 @Getter
@@ -9,7 +13,8 @@ import lombok.*;
 @AllArgsConstructor
 public class BasicSongRegisterRequestDto {
     private String basicSongFilePath;
-    private String mood;
-    private String genre;
+    private MoodType mood;
+    private GenreType genre;
     private String name;
+    private List<String> instruments;
 }
