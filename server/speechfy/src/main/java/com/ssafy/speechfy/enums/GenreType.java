0a93a38baa87cba0b54348ba851b594926cc412a
정윤선 <99yunsun@naver.com>
2025-04-07 15:14:41 +0900

    fix: fixed enums about genre and mood

--- server/speechfy/src/main/java/com/ssafy/speechfy/enums/GenreType.java
+++ server/speechfy/src/main/java/com/ssafy/speechfy/enums/GenreType.java
@@ -1,6 +1,29 @@
 package com.ssafy.speechfy.enums;
 
 public enum GenreType {
-    팝, 재즈, 힙합, 일렉트로닉, 클래식, 발라드,
-    댄스, 인디, 제이팝
+    POP("팝", "Pop"),
+    JAZZ("재즈", "Jazz"),
+    HIPHOP("힙합", "Hip-hop"),
+    ELECTRONIC("일렉트로닉", "Electronic"),
+    CLASSICAL("클래식", "Classical"),
+    BALLAD("발라드", "Ballad"),
+    DANCE("댄스", "Dance"),
+    INDIE("인디", "Indie"),
+    JPOP("제이팝", "J-Pop");
+
+    private final String korean;
+    private final String english;
+
+    GenreType(String korean, String english) {
+        this.korean = korean;
+        this.english = english;
+    }
+
+    public String getKorean() {
+        return korean;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
 }
