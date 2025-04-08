5a70b37284cfcf76fbfe2a82c871d27b49cb9855
PoeySK <piz115@naver.com>
2025-04-08 23:50:12 +0900

    fix: msw 코드 제거
    불필요한 코드로 인한 오류 발생

--- client/src/app/player/[id]/page.tsx
+++ client/src/app/player/[id]/page.tsx
@@ -1,17 +1,7 @@
 import SharePlayer from '@/components/features/SharePlayer';
-import { initializeMSW } from '@/lib/msw';
 import { getShareSong } from '@/service/apis/Share';
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
-const PlayerPage = async ({ params }: Props) => {
-  if (process.env.NODE_ENV === 'development') {
-    await initializeMSW();
-  }
+const PlayerPage = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
   const songId = Number(id);
   const data = await getShareSong(songId);
