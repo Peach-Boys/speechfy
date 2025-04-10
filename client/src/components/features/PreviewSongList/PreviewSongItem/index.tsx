d74e6e9d1bdb3f5dbb65caeace7ee0591669ad5a
강성엽 <piz115@naver.com>
2025-04-10 12:14:26 +0900

    hotfix: 삭제 로직 제거
    삭제 api가 없어서 제거

--- client/src/components/features/PreviewSongList/PreviewSongItem/index.tsx
+++ client/src/components/features/PreviewSongList/PreviewSongItem/index.tsx
@@ -8,7 +8,6 @@ import IconStop from '@/components/icons/IconStop';
 import IconTrash from '@/components/icons/IconTrash';
 import { TAGS } from '@/constants/tags';
 import { useAudioPlayBar } from '@/hooks/useAudioPlayBar';
-import { useDeleteCompletedSong } from '@/service/queries/useDeleteCompletedSong';
 import { AISong } from '@/types/song';
 import clsx from 'clsx';
 
@@ -28,12 +27,6 @@ function PreviewSongItem({ song, selected, onSelect }: Props) {
     isPlaying,
     isReady,
   } = useAudioPlayBar(song.signedUrl);
-
-  const deleteMutation = useDeleteCompletedSong(song.aiSongId);
-
-  function handleDelete() {
-    deleteMutation.mutate();
-  }
 
   return (
     <div
@@ -94,10 +87,7 @@ function PreviewSongItem({ song, selected, onSelect }: Props) {
           onSeek={(time: number) => handleSeek(time)}
         />
       </div>
-      <div
-        className='w-fit h-full flex items-center cursor-pointer hover:scale-110 transition-transform active:scale-95'
-        onClick={handleDelete}
-      >
+      <div className='w-fit h-full flex items-center cursor-pointer hover:scale-110 transition-transform active:scale-95'>
         <IconTrash width={20} height={25} color='#ff2222' />
       </div>
       <audio ref={audioRef} src={song.signedUrl} />
